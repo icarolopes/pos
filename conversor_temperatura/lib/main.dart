8e788137827727f28bdab02373445142ca6b7322
Icaro Lopes <icarofernandeslopes@gmail.com>
2019-05-11 13:22:43 -0300

    dois textField adicionada uma para celsius, e outro para Fahrenheit

--- conversor_temperatura/lib/main.dart
+++ conversor_temperatura/lib/main.dart
@@ -25,10 +25,35 @@ class _HomeState extends State<Home> {
     );
 
     Icon icon = Icon(Icons.wb_sunny, size: 120, color: Colors.yellow);
+
+    TextStyle styleDecoration = TextStyle(color: Colors.blueAccent, fontSize: 20);
+    TextStyle styleField = TextStyle(color: Colors.red);
+
+    TextField tempCelsius = TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: "Temperatura em graus Celsius",
+        labelStyle: styleDecoration,
+      ),
+      textAlign: TextAlign.center,
+      style: styleField
+    );
+
+    TextField tempFahrenheit = TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: "Temperatura em graus Fahrenheit",
+        labelStyle: styleDecoration,
+      ),
+      textAlign: TextAlign.center,
+      style: styleField
+    );
     Column column = Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget> [
         icon,
+        tempCelsius,
+        tempFahrenheit
       ],
     );
 
